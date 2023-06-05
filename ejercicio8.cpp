1b1ac72981bc4f0d6a77be07692cf6a9b98fdc56
jppuig <jppuigpesce@gmail.com>
2023-06-05 19:29:13 -0300

    archivo ej8

--- ejercicio8.cpp
+++ ejercicio8.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() {
     // IMPORTANTE! BORRAR O COMENTAR LAS SIGUIENTES LINEAS  EN TODOS LOS EJERCICIOS DEL OBLIGATORIO. NO PUEDEN ESTAR EN NINGUNA ENTREGA!
-    ifstream myFile("pruebas/Ejercicio6/100000.in.txt");
+    ifstream myFile("pruebas/Ejercicio8/10.in.txt");
     cin.rdbuf(myFile.rdbuf());
     // Si desean tirar la salida a un archivo, usen las siguientes líneas (si no, sáquenlas):
     ofstream myFile2("out.txt");
